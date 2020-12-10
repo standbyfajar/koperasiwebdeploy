dee34574ff58f9aee5319bb43cc936dae255978f
standbyfajar <fajar.karunia12.fk@gmail.com>
2020-12-10 12:42:21 +0700

    database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'koperasi',
+	'hostname' => 'db4free.net',
+	'username' => 'koperasiweb',
+	'password' => 'Cakung11',
+	'database' => 'koperasiku',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
