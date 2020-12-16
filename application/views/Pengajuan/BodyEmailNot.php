5c9a3c0ef2c3803f316f3a5a464eaa5e72d075ff
standbyfajar <fajar.karunia12.fk@gmail.com>
2020-12-16 16:43:27 +0700

    body email

--- application/views/Pengajuan/BodyEmailNot.php
+++ application/views/Pengajuan/BodyEmailNot.php
@@ -2,14 +2,13 @@
 <body>
 Jangan balas email ini.
 <p>Hai <?= $namadepan." ". $namabelakang ?>.</p>
-<p>Selamat, Pengajuan Pinjaman dengan data Pengajuan </p>
+<p>Mohon Maaf, Pengajuan Pinjaman Anda tidak dapat di setujui oleh Admin. </p>
+
 <p> No                      : <?= $notrans ?> </p>
 <p> Tanggal Pengajuan       : <?= $tgltrans ?> </p>
 <p> Tanggal Peminjaman pada : <?= $tglpinjam ?> </p>
 <p> Keterangan              : <?= $ket ?> </p>
 
-
-<p>Mohon Maaf, Pengajuan Pinjaman Anda tidak dapat di setujui oleh Admin. </p>
 <p>Silahkan Hubungi Admin Support</p>
 <br>
 Terima kasih
