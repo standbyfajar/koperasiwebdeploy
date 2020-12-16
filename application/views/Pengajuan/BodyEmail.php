a7519a99c3253e38578e0f611311d0a9b9c15b13
standbyfajar <fajar.karunia12.fk@gmail.com>
2020-12-16 15:22:51 +0700

    body email

--- application/views/Pengajuan/BodyEmail.php
+++ application/views/Pengajuan/BodyEmail.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 Jangan balas email ini.
-Hai <?= $namadepan." ". $namabelakang ?>.
+<p>Hai <?= $namadepan." ". $namabelakang ?>.</p>
 <p>Selamat, Pengajuan Pinjaman Anda sudah di setujui oleh Admin. </p>
 <p>Silahkan melanjutkan ke-Transaksi Peminjaman Uang pada App Koperasi di Android</p>
 <br>
--- application/views/Pengajuan/BodyEmailNot.php
+++ application/views/Pengajuan/BodyEmailNot.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 Jangan balas email ini.
-Hai <?= $namadepan." ". $namabelakang  ?>.
+<p>Hai <?= $namadepan." ". $namabelakang  ?>.</p>
 <p>Mohon Maaf, Pengajuan Pinjaman Anda tidak dapat di setujui oleh Admin. </p>
 <p>Silahkan Hubungi Admin Support</p>
 <br>
